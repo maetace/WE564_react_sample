953418594571a2b9a3b8b34aa2f1c3a1f205c39b
maetace <itpirain@gmail.com>
2025-03-01 16:42:22 +0700

    Setup GitHub Pages

--- src/report.jsx
+++ src/report.jsx
@@ -62,7 +62,7 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div>{/* Name */}</div>
+            <div>{" 67130893 - เมธี นิลรัตน์ "}</div>
           </Container>
         </Navbar>
         <Container>
